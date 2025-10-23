7a4544dfb28cd3031b57115f1466e27a1bbe6388
David Benavides <davirin96@gmail.com>
2025-10-23 17:45:29 -0600

    Update nextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   /* config options here */
+  experimental: {
+    outputFileTracingRoot: path.join(__dirname, '../'),
+  },
 };
 
 export default nextConfig;
